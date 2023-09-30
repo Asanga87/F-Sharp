ffccf48ca4a00a49b5b6b41b3f691ca777a8bc00
Asanga Gunarathne <asanga.it@gmail.com>
2023-10-01 02:32:34 +0530

    Add project files.

---
+++ F4/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let Main_func()=
+    
+    let rand_list =[1;2;3]
+
+    let rand_list_2 = List.map (fun x -> x * 2) rand_list
+
+    printfn "Doublie List %A" rand_list_2
+
+Main_func()
+
+Console.ReadKey() |> ignore
