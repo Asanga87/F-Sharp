bc1879f9a56da31d74f3d8d502bdead93e72bc1e
Asanga Gunarathne <asanga.it@gmail.com>
2023-10-01 18:26:27 +0530

    Update

--- F4/Program.fs
+++ F4/Program.fs
@@ -11,6 +11,7 @@ let Main_func()=
     [5;6;7;8]
     |> List.filter (fun v ->(v%2)=0)
     |> List.map (fun x-> x*2)
+    |> List.filter(fun y-> y>12)
     |> printfn"Even Double %A"
 
 Main_func()
